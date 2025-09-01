4316befdd2a3ccb05345ce36c71474f912984226
Bernd <bernd@schreinersoft.de>
2025-09-01 09:53:39 +0200

    ohne aggregation

---
+++ R_Development/@ATTIC/macros_v3.X_all_measurements.R
@@ -0,0 +1,145 @@
+print(paste("Analyzation ",modelVersion))
+print("")
+print("Using facets:")
+print(all_facets)
+print("")
+
+#source("aggregate_v2_NEO.R")
+
+data_facets <- data %>% 
+  select(all_of(all_facets)) %>% 
+  as_tibble()
+
+# Korrelationsmatrix
+cor_matrix <- cor(data_facets, use = "complete.obs")
+# Round to 2 decimal places
+cor_matrix_rounded <- round(cor_matrix, 2)
+print("Correlation Matrix:")
+print(cor_matrix_rounded)
+
+# Plot correlation Matrix
+png(paste("graphics/corrplot_", modelVersion, ".png"), width = 2400, height = 2400, res = 150)
+corrplot(cor_matrix_rounded, method = "color", type = "upper", 
+         addCoef.col = "black", tl.cex = 0.8)
+dev.off()
+
+
+# Faktorenanalyse
+faModel <- fa(data_facets, nfactors = 5, rotate = "varimax", fm = "pa", residuals=TRUE)
+print(faModel)
+
+print("Residuals:")
+print(round(faModel$residual, 2))
+print("Eigenvalues:")
+print(round(faModel$values, 2))
+
+
+# Generiere Summary für Word
+fit_summary <- data.frame(
+  RMSEA = faModel$RMSEA[1],           # RMSEA point estimate
+  RMSEA_lower = faModel$RMSEA[2],     # RMSEA lower bound (90% CI)
+  RMSEA_upper = faModel$RMSEA[3],     # RMSEA upper bound (90% CI)
+  CFI = faModel$CFI,                  # Comparative Fit Index
+  TLI = faModel$TLI,                  # Tucker-Lewis Index
+  SRMR = faModel$rms,                 # Standardized Root Mean Square Residual
+  X2 = faModel$STATISTIC,      # Chi-Square statistic
+  p = faModel$PVAL              # p-value for Chi-Square
+)
+
+fit_data <- data.frame(
+  Measure = c("RMSEA", "RMSEA 90% CI", "CFI", "TLI", "SRMR", "Chi-Square", "p-value"),
+  Value = c(
+    sprintf("%.3f", faModel$RMSEA[1]),
+    sprintf("[%.3f, %.3f]", faModel$RMSEA[2], faModel$RMSEA[3]),
+    sprintf("%.3f", faModel$CFI),
+    sprintf("%.3f", faModel$TLI), 
+    sprintf("%.3f", faModel$rms),
+    sprintf("%.2f", faModel$STATISTIC),
+    sprintf("%.3f", faModel$PVAL)
+  ),
+  Interpretation = c(
+    ifelse(faModel$RMSEA[1] <= 0.05, "Sehr gut", 
+           ifelse(faModel$RMSEA[1] <= 0.08, "Akzeptabel", "Schlecht")),
+    "",
+    ifelse(faModel$CFI >= 0.95, "Gut", "Ungeeignet"),
+    ifelse(faModel$TLI >= 0.95, "Gut", "Ungeeignet"),
+    ifelse(faModel$rms <= 0.08, "Gut", "Ungeeignet"), 
+    ifelse(faModel$PVAL >= 0.05, "Gut (nicht signifikant)", "Schlecht (signifikant)"),
+    ""
+  )
+)
+
+ft <- flextable(fit_data) %>%
+  set_header_labels(
+    Measure = "Kenngröße",
+    Value = "Value", 
+    Interpretation = "Interpretation"
+  ) %>%
+  theme_vanilla() %>%
+  align(align = "center", part = "header") %>%
+  align(j = 2, align = "center") %>%
+  width(j = 1, width = 1.5) %>%
+  width(j = 2, width = 1) %>%
+  width(j = 3, width = 1.8)
+save_as_docx(ft, path = paste("tables/measures_", modelVersion, ".docx"))
+
+
+# Generiere Faktorladungen Tabelle für Word
+loadings_matrix <- faModel$loadings[]
+
+# Convert to data frame and add item names
+loadings_df <- as.data.frame(loadings_matrix)
+loadings_df$h2 = faModel$communality
+loadings_df <- round(loadings_df, 2)  # Round to 3 decimal places
+
+# Add item names as first column (adjust item names as needed)
+loadings_df$Item <- rownames(loadings_matrix)
+loadings_df <- loadings_df[, c("Item", paste0("PA", 1:5), "h2")]  # Reorder columns
+# Row mit Eigenwerten einfügen
+eigenwerte <- round(faModel$values, 2)
+loadings_df <- loadings_df %>% 
+  add_row(Item="Eigenwert",
+          PA1 = eigenwerte[1],
+          PA2 = eigenwerte[2],
+          PA3 = eigenwerte[3],
+          PA4 = eigenwerte[4],
+          PA5 = eigenwerte[5],
+          h2 = NULL,
+          .before=1)
+
+ft <- flextable(loadings_df)
+ft <- ft %>%
+  set_header_labels(
+    Item = "",
+    PA1 = "Faktor 1",
+    PA2 = "Faktor 2", 
+    PA3 = "Faktor 3",
+    PA4 = "Faktor 4",
+    PA5 = "Faktor 5",
+    h2 = "h²"
+  ) %>%
+  theme_vanilla() %>%
+  autofit() %>%
+  align(align = "center", part = "header") %>%
+  align(j = 2:6, align = "center", part = "body") %>%
+  bold(part = "header", i = 1)
+save_as_docx(ft, path = paste("tables/loadings_", modelVersion, ".docx"))
+
+
+# Generiere Scree Plot 
+scree_data <- data.frame(
+  Component = 1:length(faModel$values),
+  Eigenvalue = faModel$values
+)
+screePlot <- scree_data %>% 
+  ggplot(aes(x = Component, y = Eigenvalue)) +
+  geom_point(size = 3) +
+  geom_line() +
+  labs(
+    x = "Primärkomponente",
+    y = "Eigenwert"
+  ) +
+  theme_minimal() +
+  scale_x_continuous(breaks = 1:length(faModel$values))
+ggsave(paste("graphics/screeplot_" , modelVersion, ".png"), plot = screePlot, dpi=300, width = 8, height = 5)
+
---
+++ R_Development/@ATTIC/publication_analyze_v3.X.R
@@ -0,0 +1,77 @@
+# Model version for printing
+modelVersion <- "v3.X"
+
+source("connect_database.R")
+source("functions.R")
+source("NEO-PI-R-Names-EN.R")
+
+#! TEST ! Analyzation of all, not aggregated measurements! -> no effect!
+
+
+# combine datasets               DANGER !!! v2 in publication is v3 XXX
+data <- tbl(con, "openai_analyzation_v2") %>% select(-updated_at) %>%
+  collect() %>% 
+  drop_na("of1")
+
+o_facets <- paste0("of", 1:6)
+c_facets <- paste0("cf", 1:6)
+e_facets <- paste0("ef", 1:6)
+a_facets <- paste0("af", 1:6)
+n_facets <- paste0("nf", 1:6)
+
+
+# remove Items from analyzation
+#o_facets <- o_facets[o_facets != "of1"] 
+#o_facets <- o_facets[o_facets != "of2"]
+#o_facets <- o_facets[o_facets != "of3"]
+
+#c_facets <- c_facets[c_facets != "cf2"]
+#c_facets <- c_facets[c_facets != "cf5"]
+#c_facets <- c_facets[c_facets != "cf6"]
+
+#e_facets <- e_facets[e_facets != "ef1"]  
+#e_facets <- e_facets[e_facets != "ef3"]  
+#e_facets <- e_facets[e_facets != "ef6"]  
+
+#a_facets <- a_facets[a_facets != "af1"] 
+#a_facets <- a_facets[a_facets != "af3"]
+#a_facets <- a_facets[a_facets != "af6"] 
+
+#n_facets <- n_facets[n_facets != "nf3"]  
+#n_facets <- n_facets[n_facets != "nf5"] 
+#n_facets <- n_facets[n_facets != "nf6"]  
+
+all_facets <- c(o_facets, c_facets, e_facets, a_facets, n_facets)
+facet_list <- list(o_facets, c_facets, e_facets, a_facets, n_facets)
+all_names <- facet_names[all_facets]
+
+
+sink(paste("outputs/output_analyzation_", modelVersion, ".txt"))
+
+source("macros_v3.X_all_measurements.R")
+
+sink()
+
+
+# CFA
+model <- '
+  Ofactor =~ of1 + of2 + of3 + of4 + of5 + of6
+  Cfactor =~ cf1 + cf2 + cf3 + cf4 + cf5 + cf6
+  Efactor =~ ef1 + ef2 + ef3 + ef4 + ef5 + ef6
+  Afactor =~ af1 + af2 + af3 + af4 + af5 + af6
+  Nfactor =~ nf1 + nf2 + nf3 + nf4 + nf5 + nf6
+'
+
+sink(paste("outputs/output_cfa_", modelVersion, ".txt"))
+fit <- cfa(model, data = data_facets, 
+           estimator = "ML")
+
+summary(fit, fit.measures = TRUE, standardized = TRUE)
+parameterEstimates(fit)
+standardizedSolution(fit)  # -1... +1
+reliability(fit) 
+inspect(fit, "cor.lv")
+modificationIndices(fit, sort = TRUE)
+residuals(fit, type = "standardized")
+
+sink()
