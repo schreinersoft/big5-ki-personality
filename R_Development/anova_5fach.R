fbfaae03b74cad6225ccd49777ca70d8bc4bb91f
Bernd <bernd@schreinersoft.de>
2025-09-10 21:27:32 +0200

    anova h-test complete

--- R_Development/anova_5fach.R
+++ R_Development/anova_5fach.R
@@ -23,13 +23,15 @@ factor_names <- c("O", "C", "E", "A", "N")
 # read all data of all models
 for (model in model_list)
 {
-  models[[model]] <- left_join(essays, db_read_model(model), by = c("id" = "essay_id")) %>% rename(essay_id = id)
+  models[[model]] <- left_join(essays, db_read_model(model), by = c("id" = "essay_id")) %>% 
+    rename(essay_id = id) %>% 
+    drop_na()
 }
 
 
 
 # Funktion zum Extrahieren der Statistiken
-extract_stats <- function(aov_result, data) {
+extract_stats_anova <- function(aov_result, data) {
   # ANOVA Zusammenfassung
   aov_summary <- summary(aov_result)
   
@@ -60,6 +62,29 @@ extract_stats <- function(aov_result, data) {
   ))
 }
 
+# Funktion zum Extrahieren der Statistiken
+extract_stats_kruskal <- function(aov_result, data) {
+  # F-Wert und p-Wert extrahieren
+  chi2_value <- aov_result$statistic
+  p_value <- aov_result$p.value
+
+  n <- nrow(data)
+  k <- kw$parameter + 1 # k = df + 1
+  
+  # Umrechnung von eta² zu Cohen's d (approximativ)
+  eta2_value <- (chi2_value - k + 1) / (n - k)
+
+  scores <- create_scores_frame(data)
+  
+  return(list(
+    p = p_value,
+    chi2 = chi2_value,
+    eta2 = eta2_value,
+    sc = mean(scores$SCORE)
+  ))
+}
+
+
 format_p_psych <- function(p_value) {
   ifelse(p_value < 0.001, "<0.001", sprintf("%.3f", p_value))
 }
@@ -85,26 +110,24 @@ for (model in model_list)
     ) %>% 
     names()
   
-  # Mit Schleife (falls viele Faktoren)
   outcomes <- c("bin_o", "bin_c", "bin_e", "bin_a", "bin_n")
   names(outcomes) <- c("O", "C", "E", "A", "N")
   
-  # Korrekte ANOVA-Syntax
   for(i in 1:5) {
     anova_results[[model]][[factor_names[i]]] <- list(
-      aov = aov(reformulate(all_factors[i], outcomes[i]), data = models[[model]]),
+      aov = aov(reformulate(outcomes[i], all_factors[i]), data = models[[model]]),
       data = models[[model]]
     )
     kruskal_wallis_results[[model]][[factor_names[i]]] <- list(
-      aov = kruskal.test(reformulate(all_factors[i], outcomes[i]), data = models[[model]]),
+      kw = kruskal.test(reformulate(outcomes[i], all_factors[i]), data = models[[model]]),
       data = models[[model]]
     )
   }
 }
 
-es <- extract_stats(anova_results[["v1.0"]][["O"]]$aov, anova_results[["v1.0"]][["O"]]$data)
+es <- extract_stats_anova(anova_results[["v1.0"]][["O"]]$aov, anova_results[["v1.0"]][["O"]]$data)
 
-kw <- kruskal_wallis_results[["v1.0"]][["O"]]$aov
+kw <- kruskal_wallis_results[["v1.0"]][["O"]]$kw
 
 
 es$p < 0.001
@@ -115,7 +138,7 @@ for(model in model_list) {
   row_data <- c(Modell = model)
   
   for(measure in factor_names) {
-    stats <- extract_stats(
+    stats <- extract_stats_anova(
       anova_results[[model]][[measure]]$aov,
       anova_results[[model]][[measure]]$data
     )
@@ -133,26 +156,25 @@ for(model in model_list) {
 }
 
 # Kruskal-Wallis-H-Test Statistiken für alle Modelle und Messgrößen extrahieren
-htest_results_df <- data.frame()
+kruskal_wallis_results_df <- data.frame()
 for(model in model_list) {
   row_data <- c(Modell = model)
   
   for(measure in factor_names) {
-    stats <- extract_stats(
-      kruskal_wallis_results[[model]][[measure]]$aov,
+    stats <- extract_stats_kruskal(
+      kruskal_wallis_results[[model]][[measure]]$kw,
       kruskal_wallis_results[[model]][[measure]]$data
     )
     
     # Statistiken formatieren
     row_data <- c(row_data,
                   format_p_psych(stats$p),
-                  sprintf("%.2f", stats$F),
-                  sprintf("%.2f", stats$d),
-                  sprintf("%.2f", stats$g),
+                  sprintf("%.2f", stats$chi2),
+                  sprintf("%.3f", stats$eta2),
                   sprintf("%.2f", stats$sc)
-    )
+                  )
   }
-  anova_results_df <- rbind(anova_results_df, row_data, stringsAsFactors = FALSE)
+  kruskal_wallis_results_df <- rbind(kruskal_wallis_results_df, row_data, stringsAsFactors = FALSE)
 }
 
 
@@ -160,11 +182,9 @@ for(model in model_list) {
 
 
 
-
+# ANOVA Auswertungen als Tabelle
 # Spaltennamen setzen
-colnames(anova_results_df) <- c("Modell",
-                          paste0(rep(factor_names, each = 5), "_", rep(c("p", "F", "d", "g", "SC"), 5)))
-
+colnames(anova_results_df) <- c("Modell", paste0(rep(factor_names, each = 5), "_", rep(c("p", "F", "d", "g", "SC"), 5)))
 # flextable erstellen
 ft <- flextable(anova_results_df) %>%
   # Unterheader für Statistiken (wird zur zweiten Zeile)
@@ -193,17 +213,41 @@ ft <- flextable(anova_results_df) %>%
 
 # Tabelle anzeigen
 print(ft)
-
 save_as_docx(ft, path=paste(tables_output_folder, "/anovas.docx",sep=""))
 
-# Alternative: Als HTML speichern
-# save_as_html(ft, path = "anova_results.html")
-
-# Für deine tatsächlichen Daten:
-# Ersetze den Simulationsteil durch deine verschachtelte Liste
-# und passe die extract_stats Funktion entsprechend an.
 
 
-#     # ... weitere Verarbeitung
-#   }
-# }
+
+# Kruskal-Wallis-H-Tests Auswertungen als Tabelle
+# Spaltennamen setzen
+colnames(kruskal_wallis_results_df) <- c("Modell", paste0(rep(factor_names, each = 4), "_", rep(c("p", "χ²", "η²", "SC"), 5)))
+# flextable erstellen
+ft <- flextable(kruskal_wallis_results_df) %>%
+  # Unterheader für Statistiken (wird zur zweiten Zeile)
+  add_header_row(values = c("Modell", rep(c("p", "χ²", "η²", "SC"), 5)), colwidths = rep(1, 21)) %>%
+  # Hauptheader hinzufügen (wird zur ersten Zeile)
+  add_header_row(values = c("", factor_names), colwidths = c(1, rep(4, 5))) %>%
+  # Erste Spalte in der ersten Zeile mit "Modell" füllen
+  #merge_v(j = 1, part = "header") %>%
+  # Layout anpassen
+  theme_box() %>%
+  # Spaltenbreiten anpassen
+  width(j = 1, width = 1.2) %>%
+  width(j = 2:21, width = 0.6) %>%
+  # Zentrierung
+  align(align = "center", part = "all") %>%
+  align(j = 1, align = "left", part = "body") %>%
+  # Schriftgröße anpassen
+  fontsize(size = 9, part = "all") %>%
+  fontsize(j = 1, size = 10, part = "body") %>%
+  # Kopfzeilen hervorheben
+  bold(part = "header") %>%
+  # Hintergrundfarben für bessere Lesbarkeit
+  bg(i = 1, part = "header", bg = "#4472C4") %>%
+  bg(i = 2, part = "header", bg = "#8DB4E2") %>%
+  color(part = "header", color = "white")
+
+# Tabelle anzeigen
+print(ft)
+save_as_docx(ft, path=paste(tables_output_folder, "/h-tests.docx",sep=""))
+
