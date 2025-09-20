3f9522502a187eb5bf53533652e386acdf5f730d
Bernd Schreiner <bernd@schreinersoft.de>
2025-09-20 16:40:41 +0200

    woolf erster wurf

--- R_Development/output_folders_measurement.R
+++ R_Development/output_folders_measurement.R
@@ -1,4 +1,4 @@
-root_folder <- "C:/Users/bernd/OneDrive/@@@APOLLON/@@Thesis KI/Auswertungen"
+root_folder <- "C:/Users/Bernd Schreiner/OneDrive/@@@APOLLON/@@Thesis KI/Auswertungen"
 tables_output_folder <- paste(root_folder, "/tables/measurement", sep="")
 graphics_output_folder <- paste(root_folder, "/graphics/measurement", sep="")
 stats_output_folder <- paste(root_folder, "/outputs/measurement", sep="")
--- R_Development/publication_measurement_woolf_min_150.R
+++ R_Development/publication_measurement_woolf_min_150.R
@@ -27,6 +27,15 @@ stats <- data %>%
   arrange(variable, author_age) %>% 
   ungroup()
 
+stats_monthly <- data %>%
+  mutate(auhor_age_months = (author_age * 12)+month) %>% 
+  group_by(auhor_age_months) %>%
+  group_modify(~ describe(select(.x, ends_with("_llm"))) %>%
+                 rownames_to_column("variable")) %>%
+  arrange(variable, auhor_age_months) %>% 
+  ungroup()
+
+
 
 # correlations
 data %>% 
@@ -41,6 +50,15 @@ mean(stats$se)
 # GRAPHICS
 create_factor_densities(data, table_name)
 
+
+# Lebensereignisse definieren
+ereignisse <- data.frame(
+  author_age = c(43, 52, 58, 59),
+  ereignis = c("Liebesbeziehung Sackville-West", "Tod Roger Fry", "Bombenangriff auf London")
+)
+
+### trendlines
+
 trendlines <- stats %>% 
   filter(author_age >34) %>% 
   mutate(
@@ -51,7 +69,7 @@ trendlines <- stats %>%
   geom_line(size = 1.0, linejoin="round") +
   geom_point(size=1.2) +
   facet_wrap(~ variable, scales = "fixed", labeller = labeller(variable = factor_names)) +
-  geom_smooth(method = "loess", linetype = "solid", alpha = 0.2, se = TRUE, size = 0.6, fill="darkgrey") +
+  geom_smooth(aes(group = variable), method = "loess", linetype = "solid", alpha = 0.2, se = TRUE, size = 0.6, fill="darkgrey") +
   scale_y_continuous(limits = c(3.8, 8), breaks = 1:9) +
   labs(
     title = "",
@@ -75,9 +93,19 @@ trendlines_flat <- stats %>%
   geom_ribbon(aes(ymin = mean - se, ymax = mean + se), alpha = 0.2, color = NA) +
   geom_line(size = 1.0, linejoin="round") +
   geom_point(size=1.2) +
-  geom_smooth(method = "loess", linetype = "dashed", alpha = 0.2, se = TRUE, size = 0.2, fill="darkgrey") +
+  geom_smooth(aes(group = variable), method = "loess", linetype = "dashed", alpha = 0.2, se = TRUE, size = 0.2, fill="darkgrey") +
+  geom_vline(data = ereignisse, 
+             aes(xintercept = author_age), 
+             color = "red", 
+             linetype = "dashed", 
+             alpha = 0.7) +
   scale_y_continuous(limits = c(3.8, 8), breaks = 1:9) +
   scale_color_discrete(name="", labels=variable_names) +
+  geom_text(data = ereignisse,
+            aes(x = author_age, y = 8, label = ereignis),
+            angle = 0, vjust = -0.3, hjust = 1,
+            size = 3, color = "red",
+            inherit.aes = FALSE) +
   guides(fill = "none") +
   labs(
     title = "",
@@ -91,8 +119,44 @@ ggsave(paste(graphics_output_folder,"/lines_flat_", table_name, ".jpg", sep = ""
        plot = trendlines_flat, dpi = 600, width = 6, height = 4)
 
 
+trendlines_annotations <- stats %>% 
+  filter(author_age >34) %>% 
+  mutate(
+    variable = factor(variable, 
+                      levels = c("o_llm", "c_llm", "e_llm", "a_llm", "n_llm"))) %>% 
+  ggplot(aes(x = author_age, y = mean, color = variable, fill = variable)) +
+  geom_ribbon(aes(ymin = mean - se, ymax = mean + se), alpha = 0.2, color = NA) +
+  geom_line(size = 1.0, linejoin="round") +
+  geom_point(size=1.2) +
+  geom_smooth(aes(group = variable), method = "loess", linetype = "dashed", alpha = 0.2, se = TRUE, size = 0.2, fill="darkgrey") +
+  geom_vline(data = ereignisse, 
+             aes(xintercept = author_age), 
+             color = "red", 
+             linetype = "dashed", 
+             alpha = 0.7) +
+  scale_y_continuous(limits = c(3.8, 8), breaks = 1:9) +
+  scale_color_discrete(name="", labels=variable_names) +
+  geom_text(data = ereignisse,
+            aes(x = author_age, y = 8, label = ereignis),
+            angle = 0, vjust = -0.3, hjust = 1,
+            size = 3, color = "red",
+            inherit.aes = FALSE) +
+  guides(fill = "none") +
+  labs(
+    title = "",
+    x = "Alter",
+    y = "M"
+  ) +
+  theme_minimal() #+
+#theme(legend.position = "none")  # Remove legend since facets show the variables
+trendlines_annotations
+ggsave(paste(graphics_output_folder,"/lines_flat_annotation_", table_name, ".jpg", sep = ""), 
+       plot = trendlines_annotations, dpi = 600, width = 6, height = 4)
 
-###### Einzele Einträge suchen
+
+1###### Einzele Einträge suchen
+
+
 e_low <- data %>% 
   filter(a_llm < 4, author_age > 57)
 
